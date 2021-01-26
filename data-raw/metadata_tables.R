54c0a3d3311b33e20353d63480966d6fff136959
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-01-26 18:34:18 -0500

    updated the L1000 representation with aliases

--- data-raw/metadata_tables.R
+++ data-raw/metadata_tables.R
@@ -1,5 +1,6 @@
 ## code to prepare `metadata_tables` dataset goes here
 
+library(org.Hs.eg.db)
 library(tidyverse)
 
 rename_columns <- function(input_names) {
@@ -26,7 +27,10 @@ cp_metadata <- read_tsv("raw/LINCS-Perturbagen-Metadata.xls") %>%
   select(-GeneTargets, -is_exemplar) %>%
   rename_with(rename_columns)
 
-l1000 <- read_tsv("raw/l1000-list.tsv")
+l1000 <- read_tsv("raw/l1000_genes.tsv", col_names = FALSE) %>%
+  pull(X1) %>%
+  unique %>%
+  AnnotationDbi::select(org.Hs.eg.db, keys = ., columns = c("ENTREZID", "ALIAS", "SYMBOL"), keytype = "SYMBOL")
 
 usethis::use_data(oe_metadata, kd_metadata, cp_metadata, l1000,
                   internal = TRUE, overwrite = TRUE)
